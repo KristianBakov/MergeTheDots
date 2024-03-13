5b3439eaa606e417fc398bab328e1417546eb16d
KristianBakov <kristian.bakov@yahoo.com>
2024-03-13 21:10:17 +0000

    Reworking grid setups cript

--- Assets/Scripts/Managers/GameplayManager.cs
+++ Assets/Scripts/Managers/GameplayManager.cs
@@ -13,10 +13,9 @@ namespace Managers
         private const int GridY = 5;
         [SerializeField] private GameObject dotPrefab;
         [SerializeField] private Transform gridParent;
-        [SerializeField] private float gridSpacing = 1.7f;
         [SerializeField] private float dotScaleFactor = 1.0f;
-
-        private Vector2 localGridScaleFactor;
+        
+        private float gridSpacing = 1.5f;
         
         public Action OnGridInitialized;
         public Dictionary<int, int> GamePointsData;
@@ -60,11 +59,12 @@ namespace Managers
             //Debug.Log(_gridDotPositions);
         }
 
-        private void GetGridScaleFactor()
+        private Vector2 GetGridScaleFactor()
         {
-            var parent = gridParent.parent;
-            localGridScaleFactor = new Vector2(gridParent.localScale.x * parent.localScale.x,
-                gridParent.localScale.y * parent.localScale.y);
+            var parentLocalScale = gridParent.parent.localScale;
+            var gridLocalScale = gridParent.localScale;
+            return new Vector2(gridLocalScale.x * parentLocalScale.x,
+                gridLocalScale.y * parentLocalScale.y);
         }
 
         //Create a 5x5 grid for the game dots
@@ -72,50 +72,80 @@ namespace Managers
         {
             int gamePointPositionCounter = 0;
             
-            GetGridScaleFactor();
             Vector2 gridPosition = gridParent.transform.position;
-            Vector2 size = _gridSpriteRenderer == null ? _gridSpriteRenderer.bounds.size :
+            Vector2 gridSize = _gridSpriteRenderer == null ? _gridSpriteRenderer.bounds.size :
                 gridParent.GetComponent<SpriteRenderer>().bounds.size;
-            Vector2 gridStartPos = new Vector2((gridPosition.x - size.x / 2f), gridPosition.y + size.y / 2f);
+            Vector2 gridStartPos = new Vector2((gridPosition.x - gridSize.x / 2f), gridPosition.y + gridSize.y / 2f);
+            Vector2 gridEndPos = new Vector2((gridPosition.x + gridSize.x / 2f), gridPosition.y - gridSize.y / 2f);
+            
             Vector2 localScale = dotPrefab.transform.lossyScale;
-            Vector2 gridDotHalfScale = new Vector2(localScale.x / 2f, localScale.y / 2f) * localGridScaleFactor;
-            //gridSpacing = size.x
+            Vector2 gridDotHalfScaleNormalized = new Vector2(localScale.x / 2f, localScale.y / 2f) * GetGridScaleFactor();
+            //CalculateGridSpacing(gridStartPos, gridDotHalfScaleNormalized, gridEndPos);
+            //if (gridSpacing > gridHaldScale)
+            //we want to adjust scale up until the space between dots is = griddotHalfScale
+
+
+            //we want to adjust scale up until the space between dots is = griddotHalfScale
             
             for (int x = 0; x < GridX; x++)
             {
                 for (int y = 0; y < GridY; y++)
                 {
                     gamePointPositionCounter++;
-
-                    float posX = gridDotHalfScale.x + (gridStartPos.x + (x * gridSpacing));
-                    float posY = -gridDotHalfScale.y + gridStartPos.y - (y * gridSpacing);
+                    
+                    // float posX = gridStartPos.x + (x * gridSpacing + x * gridDotHalfScaleNormalized.x * 2) + gridDotHalfScaleNormalized.x;
+                    // float posY = gridStartPos.y - (y * gridSpacing + y * gridDotHalfScaleNormalized.y * 2) - gridDotHalfScaleNormalized.y;
+                    float posX = gridStartPos.x + (x * gridDotHalfScaleNormalized.x + x * gridDotHalfScaleNormalized.x * 2) + gridDotHalfScaleNormalized.x;
+                    float posY = gridStartPos.y - (y * gridDotHalfScaleNormalized.y + y * gridDotHalfScaleNormalized.y * 2) - gridDotHalfScaleNormalized.y;
                     Vector2 dotPosition = new Vector3(posX, posY);
                     _gridDotPositions.TryAdd(gamePointPositionCounter, dotPosition);
                 }
             }
-            CreateDotGameObjects();
+            InitializeGridGameObjects();
             OnGridInitialized?.Invoke();
         }
 
-        public void UpdateGrid()
+        private void CalculateGridSpacing(Vector2 gridStartPos, Vector2 gridDotHalfScale, Vector2 gridEndPos)
         {
-            
+            float lastDotPosX = gridStartPos.x + (GridX * gridSpacing + GridX * gridDotHalfScale.x * 2) +
+                                gridDotHalfScale.x ;
+            if (lastDotPosX > gridEndPos.x)
+            {
+                float gridSize = gridEndPos.x - gridStartPos.x;
+                //adjust dot scale and spacing to fit the grid
+                float newSpacing = gridSize / GridX - gridDotHalfScale.x * 2;
+                gridSpacing = newSpacing;
+            }
         }
 
-        private void CreateDotGameObjects()
+        private void InitializeGridGameObjects()
         {
+
+            //calculate dot scale
+            Vector2 gridPosition = gridParent.transform.position;
+            Vector2 gridSize = _gridSpriteRenderer == null ? _gridSpriteRenderer.bounds.size :
+                gridParent.GetComponent<SpriteRenderer>().bounds.size;
+            Vector2 gridStartPos = new Vector2((gridPosition.x - gridSize.x / 2f), gridPosition.y + gridSize.y / 2f);
+            Vector2 localScale = dotPrefab.transform.lossyScale;
+            Vector2 gridDotHalfScaleNormalized = new Vector2(localScale.x / 2f, localScale.y / 2f) * GetGridScaleFactor();
+
+            gridSpacing = gridDotHalfScaleNormalized.x;
+            float newScale = _gridDotPositions[1].x - 4 * gridDotHalfScaleNormalized.x;
+            
             //TODO: Read values externally from a json/text file in format 2,4,8,16...
             foreach (var dotNum in _gridDotPositions.Keys)
             {
-                CreateDot(dotNum, 8);
+                CreateDot(dotNum, 8, newScale);
             }
         }
         
-        private void CreateDot(int pos, int value)
+        private void CreateDot(int pos, int value, float scale)
         {
             Vector3 dotPosition = new Vector3(_gridDotPositions[pos].x, _gridDotPositions[pos].y, 1);
             var dot = Instantiate(dotPrefab, dotPosition, Quaternion.identity, gridParent);
-            dot.transform.localScale = new Vector2(dotScaleFactor, dotScaleFactor) / localGridScaleFactor;
+
+            Vector2 scaleFactor = GetGridScaleFactor();
+            dot.transform.localScale += new Vector3(scale / scaleFactor.x, scale / scaleFactor.y, 1) ;
             
             _dotsGoList.Add(dot);
             GamePointsData.TryAdd(pos, value);
